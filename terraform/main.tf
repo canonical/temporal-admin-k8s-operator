990cb4cbe7553103b38d43a115e3886c567b8009
jananisenthilkumar291 <60182776+jananisenthilkumar291@users.noreply.github.com>
2025-12-09 14:24:50 -0500

    [WF-203] Use juju provider v1.0.0 (#44)
    * Update juju provider to v1
    
    * Updated juju provider to v1.0.0 and added juju_model data source to dynamically fetch model_uuid
    
    * Updated data resource so that model name could be an input from user
    
    * Updated to mandate user input model_uuid
    
    * Remove model_uuid from test/test.tfvars
    
    * Removing model_uuid from test.tfvars on model destroy
    
    ---------
    
    Co-authored-by: Shayan Patel <shayan.patel@canonical.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "juju_application" "temporal_admin_k8s" {
-  name  = var.app_name
-  model = var.model
+  name       = var.app_name
+  model_uuid = var.model_uuid
 
   charm {
     name     = "temporal-admin-k8s"
