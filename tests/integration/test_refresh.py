8ea2150e4033e3a5aaf3af5e2d9df50d76e6d164
Daniela Plascencia <daniela.plascencia@canonical.com>
2026-07-15 16:36:08 -0600

    chore(tests): bump version to deploy/refresh from
    Due to the recent changes in the Temporal workload version,
    the refresh integration test must verify the upgrade path
    from a previous version to the newer one; therefore, this
    commit bumps the version from which we are deploying and
    refreshing from (replace latest/stable with 1.23/stable).

--- tests/integration/test_refresh.py
+++ tests/integration/test_refresh.py
@@ -1,14 +1,14 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Test to ensure successful refreshes from latest track to the 1.23 track."""
+"""Test to ensure successful refreshes from the latest supported release to the newer charm."""
 
 import jubilant
 from conftest import TEMPORAL_SERVER_APP_NAME
 
 
 def test_refresh_from_latest_to_1_23(juju: jubilant.Juju, admin_tools_latest_track, charm_path, charm_resources):
-    """Test to refresh from latest track to the 1.23 track."""
+    """Refresh from the latest supported temporal-admin-k8s release to the local build."""
     juju.refresh(
         admin_tools_latest_track,
         path=charm_path,
